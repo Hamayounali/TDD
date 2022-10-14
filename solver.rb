8617577376cc10159918f47d96aa7a2ab73ab470
Collins <mussocollins@gmail.com>
2022-10-14 13:14:08 +0300

    Add the factorial method

--- solver.rb
+++ solver.rb
@@ -4,4 +4,18 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def factorial(int)
+    result = 1
+    result if int.zero?
+
+    raise 'integer must be positive' if int.negative?
+
+    while int.positive?
+      result *= int
+      int -= 1
+    end
+    result
+  end
+
 end

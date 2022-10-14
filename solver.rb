b7b2e3ed5e168d2d2f4f73334b012d764b24d584
Collins <mussocollins@gmail.com>
2022-10-14 13:02:00 +0300

    Add reverse method

---
+++ solver.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Solver
+  def reverse(word)
+    word.reverse
+  end
+end
--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require_relative '../solver'
 
 describe Solver do
   context 'When testing the Solver class' do
     solver = Solver.new
     it 'test the revers method with word = hello to return olleh' do
-      result = solver.revers("hello")
+      result = solver.reverse('hello')
       expect(result).to eq 'olleh'
     end
   end

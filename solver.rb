435de6be98acc618aea21ea8a20b74305c33e1d3
Hamayoun <humayoonali2@gmail.com>
2022-10-14 14:49:27 +0430

    Created fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -18,4 +18,16 @@ class Solver
     result
   end
 
+  def fizz_buzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
+
 end
